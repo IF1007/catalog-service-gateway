53e8f5feee86c02d900c8a35121dc569ae9b99b6
arthur pires <a>
2018-05-15 21:58:22 -0300

    Microservice base in go

---
+++ service/server.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+)
+
+func verifyToken(resp http.ResponseWriter, req *http.Request) {
+	// verify token, TODO: create a package for token generate / verify
+	isTokenValid := true
+
+	// TODO: Add route mapping
+	if isTokenValid {
+		// TODO: redirect for corresponding service
+		resp.Write([]byte("sucess " + req.URL.Path))
+	} else {
+		// return 401
+		resp.WriteHeader(401)
+		resp.Write([]byte("Invalid Token"))
+	}
+
+	// TODO: read about defer
+}
+
+func main() {
+	argsProgram := os.Args[1:]
+
+	// TODO: add support for multiple versions
+	version := argsProgram[0]
+
+	router := mux.NewRouter()
+	router.PathPrefix("/"+version).Methods("GET", "PUT", "DELETE", "POST").HandlerFunc(verifyToken)
+
+	// TODO: define requests timeout
+	if err := http.ListenAndServe(":8080", router); err != nil {
+		panic(err)
+	}
+}
